63ce51c4006c6c726767f0c0ad1cb5c60c9faf8c
Syncib <saqibabbas@live.co.uk>
2024-11-15 15:23:12 +0500

    "Refactored App.css and App.js to update styles and layout, replacing logo with a new image and adding a blog post layout with multiple entries."

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import dumimg from "./assets/images/blog-1.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+          <img src={dumimg} alt="imge" />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Saqib Abbas</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src={dumimg} alt="imge" />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Saqib Abbas</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src={dumimg} alt="imge" />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Saqib Abbas</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src={dumimg} alt="imge" />
+        </div>
+        <div className="texts">
+          <h2>Full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">Saqib Abbas</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
