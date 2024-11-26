f1bd607b2f9323c46e81732aa87c2b38b8ca6cc9
Syncib <saqibabbas@live.co.uk>
2024-11-26 16:06:22 +0500

    "Updated fetch URL in EditPost component to include '/post/' prefix"

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('https://blog-server-dun.vercel.app/'+id)
+    fetch('https://blog-server-dun.vercel.app/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
